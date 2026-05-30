60faeeb128ef4ac3f2f90ba784637c8af9c97ecb
Boris Kantor <boris@kantor-online.de>
2026-05-30 11:58:18 +0200

    added ios app

---
+++ iOSApp/openEyes/openEyes/openEyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  openEyesApp.swift
+//  openEyes
+//
+//  Created by Boris Kantor on 30.05.26.
+//
+
+import SwiftUI
+
+@main
+struct openEyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
